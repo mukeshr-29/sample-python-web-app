07dec4540fb7c86e1fb9b01f1149a894cea22d01
mukeshr-29 <mukeshr2911@gmail.com>
2023-08-09 17:42:45 +0530

    update sample application

--- lib/ec2-cdk-stack.ts
+++ lib/ec2-cdk-stack.ts
@@ -1,18 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
-import { readFileSync } from 'fs';
 import { Construct } from 'constructs';
+import { readFileSync } from 'fs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export class Ec2CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
-}
 import { Vpc, SubnetType, Peer, Port, AmazonLinuxGeneration, 
   AmazonLinuxCpuType, Instance, SecurityGroup, AmazonLinuxImage,
   InstanceClass, InstanceSize, InstanceType
 } from 'aws-cdk-lib/aws-ec2';
+    // The code that defines your stack goes here
 
+    // example resource
+    // const queue = new sqs.Queue(this, 'Ec2CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
 import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { Pipeline, Artifact } from 'aws-cdk-lib/aws-codepipeline';
 import { GitHubSourceAction, CodeBuildAction, CodeDeployServerDeployAction } from 'aws-cdk-lib/aws-codepipeline-actions';
@@ -20,13 +19,9 @@ import { PipelineProject, LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 import { ServerDeploymentGroup, ServerApplication, InstanceTagSet } from 'aws-cdk-lib/aws-codedeploy';
 import { SecretValue } from 'aws-cdk-lib';
 
-export class PythonEc2BlogpostStack extends cdk.Stack {
+export class Ec2CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // IAM
-    // Policy for CodeDeploy bucket access
-    // Role that will be attached to the EC2 instance so it can be 
-    // managed by AWS SSM
     const webServerRole = new Role(this, "ec2Role", {
       assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
     });
@@ -35,13 +30,12 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
     webServerRole.addManagedPolicy(
       ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
     );
-    
+
     webServerRole.addManagedPolicy(
       ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonEC2RoleforAWSCodeDeploy")
     );
 
-    // VPC
-    // This VPC has 3 public subnets, and that's it
+        // This VPC has 3 public subnets, and that's it
     const vpc = new Vpc(this, 'main_vpc',{
       subnetConfiguration: [
         {
@@ -62,22 +56,20 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
       ]
     });
 
-    // Security Groups
+        // Security Groups
     // This SG will only allow HTTP traffic to the Web server
     const webSg = new SecurityGroup(this, 'web_sg',{
       vpc,
       description: "Allows Inbound HTTP traffic to the web server.",
       allowAllOutbound: true,
     });
-    
+
     webSg.addIngressRule(
       Peer.anyIpv4(),
       Port.tcp(80)
     );
-    
-    // EC2 Instance
-    // This is the Python Web server that we will be using
-    // Get the latest AmazonLinux 2 AMI for the given region
+
+        // the AMI to be used for the EC2 Instance
     const ami = new AmazonLinuxImage({
       generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
       cpuType: AmazonLinuxCpuType.X86_64,
@@ -87,7 +79,7 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
     const webServer = new Instance(this, 'web_server',{
       vpc,
       instanceType: InstanceType.of(
-        InstanceClass.T3,
+        InstanceClass.T2,
         InstanceSize.MICRO,
       ),
       machineImage: ami,
@@ -101,37 +93,35 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
     // Tag the instance
     cdk.Tags.of(webServer).add('application-name','python-web')
     cdk.Tags.of(webServer).add('stage','prod')
-    
-    // Pipeline stuff
-    // CodePipeline
-    const pipeline = new Pipeline(this, 'python_web_pipeline', {
+
+        // CodePipeline
+    const pipeline = new Pipeline(this, 'python_web_pipeline',{
       pipelineName: 'python-webApp',
       crossAccountKeys: false, // solves the encrypted bucket issue
     });
-
-    // STAGES
-    // Source Stage
+    
+        // STAGES
+        // Source Stage
     const sourceStage = pipeline.addStage({
       stageName: 'Source',
-    });
-    
-    // Build Stage
+    })
+        
+        // Build Stage
     const buildStage = pipeline.addStage({
       stageName: 'Build',
-    });
-    
-    // Deploy Stage
+    })
+        
+        // Deploy Stage
     const deployStage = pipeline.addStage({
       stageName: 'Deploy',
-    });
-
-    // Add some action
+    })
+    
     // Source action
     const sourceOutput = new Artifact();
     const githubSourceAction = new GitHubSourceAction({
       actionName: 'GithubSource',
-      oauthToken: SecretValue.secretsManager('github-oauth-token'), // SET UP BEFORE
-      owner: 'mukeshr-29', // THIS NEEDS TO BE CHANGED TO THE READER
+      oauthToken: SecretValue.secretsManager('github-oauth-token'), // MAKE SURE TO SET UP BEFORE
+      owner: 'mukeshr-29', // THIS NEEDS TO BE CHANGED TO YOUR OWN USER ID
       repo: 'sample-python-web-app',
       branch: 'main',
       output: sourceOutput,
@@ -140,13 +130,14 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
     sourceStage.addAction(githubSourceAction);
 
     // Build Action
-    const pythonTestProject = new PipelineProject(this, 'pythonTestProject', {
+    const pythonTestProject = new PipelineProject(this, 'pythonTestProject',{
       environment: {
         buildImage: LinuxBuildImage.AMAZON_LINUX_2_3
       }
     });
-    
+
     const pythonTestOutput = new Artifact();
+
     const pythonTestAction = new CodeBuildAction({
       actionName: 'TestPython',
       project: pythonTestProject,
@@ -155,14 +146,13 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
     });
 
     buildStage.addAction(pythonTestAction);
-
     // Deploy Actions
-    const pythonDeployApplication = new ServerApplication(this,"python_deploy_application", {
+    const pythonDeployApplication = new ServerApplication(this,"python_deploy_application",{
       applicationName: 'python-webApp'
     });
 
     // Deployment group
-    const pythonServerDeploymentGroup = new ServerDeploymentGroup(this,'PythonAppDeployGroup', {
+    const pythonServerDeploymentGroup = new ServerDeploymentGroup(this,'PythonAppDeployGroup',{
       application: pythonDeployApplication,
       deploymentGroupName: 'PythonAppDeploymentGroup',
       installAgent: true,
@@ -182,10 +172,27 @@ export class PythonEc2BlogpostStack extends cdk.Stack {
 
     deployStage.addAction(pythonDeployAction);
 
-    // Output the public IP address of the EC2 instance
-    new cdk.CfnOutput(this, "IP Address", {
-      value: webServer.instancePublicIp,
-    });
+  // Output the public IP address of the EC2 instance
+  new cdk.CfnOutput(this, "IP Address", {
+    value: webServer.instancePublicIp,
+  });
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
